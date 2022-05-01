71cc2473321ef2013c9325cdf6ff0e82065eb761
Stefan <denik1999@ukr.net>
2022-05-01 22:39:36 +0300

    Added media folder, image display, Scan and rotate buttons and func for rotate btn

--- web_table_parser/view/urls.py
+++ web_table_parser/view/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from view import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница.
     path('', views.index, name='index'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web_table_parser/view/views.py
+++ web_table_parser/view/views.py
@@ -1,18 +1,27 @@
+import cv2
+
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
+from web_table_parser.settings import MEDIA_URL, MEDIA_ROOT
 
 
 def index(request):
     if request.method == "POST":
-        print(request.POST)
-        print("Files: ", request.FILES)
-        upload = request.FILES['input-b1']
-        fss = FileSystemStorage()
-        file = fss.save(upload.name, upload)
-        print(file)
-    # if request.method == "GET":
-    #     if request.user.is_authenticated:
-    #         tasks_list = Task.objects.filter(user=request.user.id)
-    #         return render(request, 'todolistapp/index.html', {'tasks_list': tasks_list})
+        print("POST ", request.POST)
+        if 'rotate' in request.POST:
+            cv_image = cv2.imread(f'{MEDIA_ROOT}/{request.POST["src"].split("/")[-1]}')
+            cv_image = cv2.rotate(cv_image, cv2.ROTATE_90_CLOCKWISE)
+            saved_image = f'{MEDIA_ROOT}/saved_image.png'
+            print('saved image: ', saved_image)
+            cv2.imwrite(saved_image, cv_image)
+            return render(request, 'view/index.html', context={'src': f'{MEDIA_URL}/saved_image.png'})
+        if 'input-b1' in request.FILES:
+            upload = request.FILES['input-b1']
+            fss = FileSystemStorage()
+            file = fss.save(upload.name, upload)
+            return render(request, 'view/index.html', context={'src': f'{MEDIA_URL}{file}'})
+        elif 'src' in request.POST:
+            return render(request, 'view/index.html', context={'src': request.POST['src']})
+
     return render(request, 'view/index.html')
